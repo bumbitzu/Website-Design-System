c9de4724556af70c30163847cc8a37819c6254a6
Adrian Bumbu <adibumbu91@gmail.com>
2023-12-10 12:31:53 +0200

    update

--- test.js
+++
@@ -1,8 +0,0 @@
-// Șirul original
-var sirOriginal = "./res/html/menus/pile-navigation.html";
-
-// Modificăm ultimele 5 caractere
-var sirModificat = sirOriginal.slice(0, -5) + "-code.html";
-
-// Afișăm rezultatul
-console.log(sirModificat);
